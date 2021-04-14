d0430da8bf46f853296f61a71d4f0f94e67ccbd7
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-14 16:47:23 -0400

    feat: Add `Key` class

---
+++ include/key.h
@@ -0,0 +1,26 @@
+/*
+ * key.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef KEY_H
+#define KEY_H
+
+class Key
+{
+	public:
+		Key();
+		virtual ~Key();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* KEY_H */ 
