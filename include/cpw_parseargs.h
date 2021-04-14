3937fa12804a078fcd0f5d4c07a05e34a102ebe2
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-14 16:46:20 -0400

    feat: Add the `CPWParseArgs` class

---
+++ include/cpw_parseargs.h
@@ -0,0 +1,26 @@
+/*
+ * cpw_parseargs.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef CPW_PARSEARGS_H
+#define CPW_PARSEARGS_H
+
+class CPWParseArgs
+{
+	public:
+		CPWParseArgs();
+		virtual ~CPWParseArgs();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* CPW_PARSEARGS_H */ 
---
+++ src/cpw_parseargs.cc
@@ -0,0 +1,26 @@
+/*
+ * cpw_parseargs.cc
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#include "cpw_parseargs.h"
+
+
+CPWParseArgs::CPWParseArgs()
+{
+	
+}
+
+
+CPWParseArgs::~CPWParseArgs()
+{
+	
+}
+
