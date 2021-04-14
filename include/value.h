14898fb1b483c610d2dcf3d037e41b3938bf8c69
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-14 16:48:45 -0400

    feat: Add `Value` class

---
+++ include/value.h
@@ -0,0 +1,26 @@
+/*
+ * value.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef VALUE_H
+#define VALUE_H
+
+class Value
+{
+	public:
+		Value();
+		virtual ~Value();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* VALUE_H */ 
