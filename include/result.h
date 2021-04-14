c8b0ff1dbfb92035ac6b0eb962f50aab5b80fff9
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-14 16:48:24 -0400

    feat: Add `Result` class

---
+++ include/result.h
@@ -0,0 +1,26 @@
+/*
+ * result.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef RESULT_H
+#define RESULT_H
+
+class Result
+{
+	public:
+		Result();
+		virtual ~Result();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* RESULT_H */ 
