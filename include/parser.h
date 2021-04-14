00dc365897e94588998da7777b5d883e96327e58
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-14 16:47:53 -0400

    feat: Add `Parser` class

---
+++ include/parser.h
@@ -0,0 +1,26 @@
+/*
+ * parser.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef PARSER_H
+#define PARSER_H
+
+class Parser
+{
+	public:
+		Parser();
+		virtual ~Parser();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* PARSER_H */ 
