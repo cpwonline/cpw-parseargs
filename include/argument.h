6498961b0645566c2d4fb3e182b006b8ae253dc9
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-14 16:46:49 -0400

    feat: Add `Argument` class

---
+++ include/argument.h
@@ -0,0 +1,26 @@
+/*
+ * argument.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef ARGUMENT_H
+#define ARGUMENT_H
+
+class Argument
+{
+	public:
+		Argument();
+		virtual ~Argument();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* ARGUMENT_H */ 
