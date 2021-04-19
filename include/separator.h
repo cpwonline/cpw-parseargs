ff9c67c734d69d31b99765fd570ee9ac25a69678
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-19 13:15:47 -0400

    feat: Add `Separator` class

---
+++ include/separator.h
@@ -0,0 +1,26 @@
+/*
+ * separator.h
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#ifndef SEPARATOR_H
+#define SEPARATOR_H
+
+class Separator
+{
+	public:
+		Separator();
+		virtual ~Separator();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* SEPARATOR_H */ 
