74e21298afa77473b54c907a65083031b93787de
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-04-19 13:17:25 -0400

    feat: Add `Comparator` class

---
+++ src/comparator.cc
@@ -0,0 +1,26 @@
+/*
+ * comparator.cc
+ * 
+ * Copyright 2021 CPW Online <support@cpwonline.org>
+ * 
+ * This program is free software; see the License file for more details.
+ * 
+ * You should have received a copy of the License along with this program.
+ * 
+ */
+
+
+#include "comparator.h"
+
+
+Comparator::Comparator()
+{
+	
+}
+
+
+Comparator::~Comparator()
+{
+	
+}
+
